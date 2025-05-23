cf594b0b3cc4a80babfcaabb2b57d34d67f1364a
OfficiallyDragon <dexterwhetcroft@gmail.com>
2025-05-23 16:30:09 +0100

    feat: implement send-results command with win/lose reporting and map images

---
+++ commands/utility/send-results.js
@@ -0,0 +1,88 @@
+/* eslint-disable brace-style */
+const { SlashCommandBuilder, MessageFlags, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('send-results')
+		.setDescription('Sets up the results channel for the server.')
+		.addBooleanOption(option =>
+			option.setName('win')
+				.setDescription('did you win?')
+				.setRequired(true),
+		)
+		.addStringOption(option =>
+			option.setName('score')
+				.setDescription('How much did you win/lose by?')
+				.setRequired(true),
+		)
+		.addStringOption(option =>
+			option.setName('current-points')
+				.setDescription('how many points do you have after the win or lose?')
+				.setRequired(true),
+		)
+		.addStringOption(option =>
+			option.setName('map')
+				.setDescription('What map did you play?')
+				.setRequired(true)
+				.addChoices(
+					{ name: 'Ascent', value: 'Ascent' },
+					{ name: 'Haven', value: 'Haven' },
+					{ name: 'Split', value: 'Split' },
+					{ name: 'Icebox', value: 'Icebox' },
+					{ name: 'Sunset', value: 'Sunset' },
+					{ name: 'Pearl', value: 'Pearl' },
+					{ name: 'Lotus', value: 'Lotus' },
+					{ name: 'Fracture', value: 'Fracture' },
+					{ name: 'Abyss', value: 'Abyss' },
+					{ name: 'Bind', value: 'Bind' },
+					{ name: 'Breeze', value: 'Breeze' },
+				),
+		),
+	async execute(interaction) {
+
+		const mapImages = {
+			'Ascent': 'https://static.wikia.nocookie.net/valorant/images/e/e7/Loading_Screen_Ascent.png',
+			'Haven': 'https://static.wikia.nocookie.net/valorant/images/7/70/Loading_Screen_Haven.png',
+			'Split': 'https://static.wikia.nocookie.net/valorant/images/d/d6/Loading_Screen_Split.png',
+			'Icebox': 'https://static.wikia.nocookie.net/valorant/images/1/13/Loading_Screen_Icebox.png',
+			'Sunset': 'https://static.wikia.nocookie.net/valorant/images/5/5c/Loading_Screen_Sunset.png',
+			'Pearl': 'https://static.wikia.nocookie.net/valorant/images/a/af/Loading_Screen_Pearl.png',
+			'Lotus': 'https://static.wikia.nocookie.net/valorant/images/d/d0/Loading_Screen_Lotus.png',
+			'Fracture': 'https://static.wikia.nocookie.net/valorant/images/f/fc/Loading_Screen_Fracture.png',
+			'Abyss': 'https://static.wikia.nocookie.net/valorant/images/6/61/Loading_Screen_Abyss.png',
+			'Bind': 'https://static.wikia.nocookie.net/valorant/images/2/23/Loading_Screen_Bind.png',
+			'Breeze': 'https://static.wikia.nocookie.net/valorant/images/1/10/Loading_Screen_Breeze.png',
+		};
+
+		const winEmbed = new EmbedBuilder()
+			.setColor('#5ef281')
+			.setTitle('We won a premier match!')
+			.setDescription(`And now have **${interaction.options.getString('current-points')}** points
+            Score: ${interaction.options.getString('score')}
+
+            On the map **${interaction.options.getString('map')}**`);
+
+		const loseEmbed = new EmbedBuilder()
+			.setColor('#c53425')
+			.setTitle('We lost a premier match :(')
+			.setDescription(`And now have **${interaction.options.getString('current-points')}** points
+            Score: ${interaction.options.getString('score')}
+            
+            On the map **${interaction.options.getString('map')}**`);
+
+		const selectedMap = interaction.options.getString('map');
+		const mapImage = mapImages[selectedMap];
+
+		if (mapImage) {
+			winEmbed.setImage(mapImage);
+			loseEmbed.setImage(mapImage);
+		}
+
+		await interaction.reply({ content: 'Results sent successfully!', flags: MessageFlags.Ephemeral });
+		if (interaction.options.getBoolean('win')) {
+			await interaction.channel.send({ embeds: [winEmbed] });
+		} else {
+			await interaction.channel.send({ embeds: [loseEmbed] });
+		}
+	},
+};
